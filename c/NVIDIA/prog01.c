d740b4672b8b30ba3ee319fdd61e1e6197708cf0
Praveen Pandit <pandit.praveen@gmail.com>
2020-06-27 00:06:22 +0530

    Facebook Interview Challenges

--- c/NVIDIA/prog01.c
+++ c/NVIDIA/prog01.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+int avg(int a, int b)
+{
+    return (a / 2) + (b / 2) + ((a & 1) ^ (b & 1) ^ 1);
+}
 int main()
 {
+    int a = 3;
+    int b = 5;
+    printf("%d \n", avg(a, b));
     char s[] = "hello%ok";
     printf("%s\n", s);
     printf(s);
